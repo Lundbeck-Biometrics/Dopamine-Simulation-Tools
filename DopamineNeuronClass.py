d652b18419c45eed0631cd3a77e448b3d8c7f8c9
jakobdreyer <jakd@lundbeck.com>
2018-02-28 08:36:57 +0100

    Added Dopamine class... again?

--- DopamineNeuronClass.py
+++ DopamineNeuronClass.py
@@ -27,18 +27,19 @@ class DA:
     """This is a dopamine class. Sets up a set of eqns that represent DA. Parameters depend on area. 
     Call like DA(""VTA"") or DA(""SNC""). Area argument is not case sensitive"""
     def __init__(self, area = "VTA"):
-        self.D2term = feedback(2, 1e-2, 0.4)
+        self.D2term = feedback(5, 1e-2, 0.4)
         self.D2soma = feedback(0.8, 0.2, 0.6, 0)
         Original_NNeurons = 100;
         self.NNeurons = Original_NNeurons;
-        self.NU_in = 5.0; #This is the parameter that determines DA levels
+#        self.NU_in = 5.0; #This is the parameter that determines DA levels
+        self.nu = 0; "This attribute reports the current firing rate after sd feedback"
         
         if area.lower() == "vta":
             self.Vmax_pr_neuron = 1500.0/Original_NNeurons
-            self.Gamma_pr_neuron = 2.0/Original_NNeurons #This value is deliberately high and anticipates a ~50% reduction by terminal feedback
+            self.Gamma_pr_neuron = 200.0/Original_NNeurons #This value is deliberately high and anticipates a ~50% reduction by terminal feedback
         elif area.lower() == "snc":
             self.Vmax_pr_neuron = 4000.0/Original_NNeurons
-            self.Gamma_pr_neuron = 4.0/Original_NNeurons; 
+            self.Gamma_pr_neuron = 400.0/Original_NNeurons; 
         else:
             print('You are in unknown territory')
         self.Vmax_pr_neuron_soma = 200.0/Original_NNeurons;
@@ -56,9 +57,9 @@ class DA:
         self.D2soma.update(dt, self.Conc_DA_soma)
         self.D2term.update(dt, self.Conc_DA_term)
         
-        nu = nu_in*self.D2soma.gain;
+        self.nu = nu_in*self.D2soma.gain;
 #        print('input: ', self.NU_in, '. Effective:' , nu)
-        rel = np.random.poisson(self.NNeurons*nu*dt);
+        rel = np.random.poisson(self.NNeurons*self.nu*dt);
         "first calculate somato dendritic DA:"
         self.Conc_DA_soma += rel*self.Gamma_pr_neuron_soma - dt*self.Vmax_pr_neuron_soma*self.NNeurons*self.Conc_DA_soma/(self.Km + self.Conc_DA_soma) - dt*self.k_nonDAT;
         "...then terminal DA"
@@ -69,27 +70,35 @@ class DA:
 #test the classes:
 test = DA('vta')
 #create a list of class instances: 
-nlist = 3;
+nlist = 2;
 DAlist = [DA('SNC') for i in range(nlist)]
-DAlevels = [10, 30, 2309]
-Nupdates = int(1e5);
-Occ = np.zeros([nlist, Nupdates]);
-dt = 0.001;
+Nupdates = int(1e4);
+C_DA = np.zeros([nlist, Nupdates]);
+NUreal = np.zeros([nlist, Nupdates]);
+D2Term = np.zeros([nlist, Nupdates]);
+
+
+dt = 0.005;
 timeax = dt*np.arange(0,Nupdates);
-NU = 5*np.ones(Nupdates);
+
+"generating firing rate"
+NU = 5*np.ones([nlist, Nupdates]);
 burstindx = np.logical_and(20 <= timeax, timeax < 25);
-NU[burstindx]= 20
+NU[0,burstindx]= 20
 
 burstindx = np.logical_and(40 <= timeax, timeax < 45);
-NU[burstindx]= 0
+NU[0,burstindx]= 0
 
 
 for k in range(nlist):
+    
     DAlist[k].NNeurons = 100 - k*40
 for L in range(Nupdates):
     for k in range(nlist):
-        DAlist[k].update(dt, NU[L])
-        Occ[k,L]= DAlist[k].Conc_DA_term
+        DAlist[k].update(dt, NU[k,L])
+        C_DA[k,L]= DAlist[k].Conc_DA_term
+        NUreal[k,L]= DAlist[k].nu
+        D2Term[k,L]= DAlist[k].D2term.gain
 
 
 
@@ -97,7 +106,21 @@ fignum = 1;
 plt.close(fignum)
 plt.figure(fignum);
 for k in range(nlist):
-    plt.plot(timeax, Occ[k])
+    plt.plot(timeax, C_DA[k])
+
+
+fignum = 2;
+plt.close(fignum)
+plt.figure(fignum);
+for k in range(nlist):
+    plt.plot(timeax, NUreal[k])
+
+
+fignum = 3;
+plt.close(fignum)
+plt.figure(fignum);
+for k in range(nlist):
+    plt.plot(timeax, D2Term[k])
 
 
 
