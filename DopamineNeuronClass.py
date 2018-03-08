23132a4f341925c4d3f7bffcccf59cd289d54000
jakobdreyer <jakd@lundbeck.com>
2018-03-08 10:21:58 +0100

    updated feedbacks

--- DopamineNeuronClass.py
+++ DopamineNeuronClass.py
@@ -7,52 +7,38 @@ Created on Mon Feb 26 09:32:34 2018
 
 import numpy as np
 
-
+import matplotlib.pyplot as plt
 
 class feedback:
-    "This is a feedback class"
-    def __init__(self, alpha, k_on, k_off, Gmin = 0, occupancy = 0.5):
+    """This is a feedback class. Takes arguments: 
+        alpha: list of two numbers
+        k_on: onrate, typically 1e-2
+        k_off: offrate, 1 or 10 or whatever. 
+        """
+    def __init__(self, alpha, k_on, k_off, occupancy = 0.5):
         "The feedback. "
         self.alpha = alpha;
         self.k_on = k_on;
         self.k_off = k_off;
         self.occupancy = occupancy;
-        self.Gmin = Gmin;
         self.update(0,0);#This call makes shure that the 'gain' attribute gets set
         
     def update(self, dt, C):
         self.occupancy += dt*( (1 - self.occupancy)*self.k_on*C - self.occupancy*self.k_off) 
         self.occupancy = np.max([0, self.occupancy])
         "Gain is defined so that occupancy = 0, means gain = 1. "
-        self.gain =  (self.Gmin + 1)/(1+self.alpha*self.occupancy) - self.Gmin
+        self.gain = bool(self.alpha[0])/(1+self.alpha[0]*self.occupancy) + self.alpha[1]*self.occupancy
         "Make sure gain is bigger than 0. "
         self.gain = np.max([0, self.gain]);
     
-#class feedback_keep:
-#    "This is a feedback class"
-#    def __init__(self, alpha, k_on, k_off, occupancy = 0.5):
-#        self.alpha = alpha;
-#        self.k_on = k_on;
-#        self.k_off = k_off;
-#        self.occupancy = occupancy;
-#        self.update(0,0);#This call makes shure that the 'gain' attribute gets set
-#        
-#    def update(self, dt, C):
-#        self.occupancy += dt*( (1 - self.occupancy)*self.k_on*C - self.occupancy*self.k_off) 
-#        self.occupancy = np.max([0, self.occupancy])
-#        self.gain =  1/(1+self.alpha*self.occupancy)
-#    
  
-      
-    
 class DA:
     """This is a dopamine class. Sets up a set of eqns that represent DA. Parameters depend on area. 
     Create instances by calling DA(""VTA"") or DA(""SNC""). Area argument is not case sensitive.
     Update method uses forward euler steps. Works for dt < 0.005 s"""
     def __init__(self, area = "VTA"):
-        feedrate = 0.3;
-        self.D2term = feedback(3, feedrate*1e-2, feedrate*1, 0)
-        self.D2soma = feedback(0.5, 1e-2, 10, 4, 0)
+        self.D2term = feedback([3.,0.], 0.3e-2, 0.3)
+        self.D2soma = feedback([0, 10], 1e-2, 10, 0)
         Original_NNeurons = 100;
         self.NNeurons = Original_NNeurons;
 #        self.NU_in = 5.0; #This is the parameter that determines DA levels
@@ -81,7 +67,9 @@ class DA:
         self.D2soma.update(dt, self.Conc_DA_soma)
         self.D2term.update(dt, self.Conc_DA_term)
         "If e_stim -> True, then the firing rate overrules s.d. inhibition"
-        self.nu = nu_in*self.D2soma.gain*int(not(e_stim)) + nu_in*int(e_stim);
+        
+        self.nu = np.max([nu_in - self.D2soma.gain*(1 - e_stim), 0]);
+#       self.nu = nu_in*self.D2soma.gain*int(not(e_stim)) + nu_in*int(e_stim);
 #        print('input: ', self.NU_in, '. Effective:' , nu)
         rel = np.random.poisson(self.NNeurons*self.nu*dt);
         "first calculate somato dendritic DA:"
@@ -89,3 +77,35 @@ class DA:
         "...then terminal DA"
         self.Conc_DA_term += self.Precurser*rel*self.Gamma_pr_neuron*self.D2term.gain - dt*self.Vmax_pr_neuron*self.NNeurons*self.Conc_DA_term/(self.Km + self.Conc_DA_term)  - dt*self.k_nonDAT;
          
+VTA_da = DA('vta'); #Here we create onstance of the DA class!!
+
+
+Nupdates = 4000; #number of updates for the simulation
+dt = 0.005; #Timestep!
+t = dt*np.arange(0,Nupdates);#time axis ffor plotting. 
+
+"Create results vectors"
+VTA_C = np.zeros(Nupdates)
+NAc_C = np.zeros(Nupdates)
+NU = np.zeros(Nupdates)
+
+for k in range(Nupdates):
+    VTA_da.update(dt);# default firing rate is 5Hz input.
+    VTA_C[k] = VTA_da.Conc_DA_soma
+    NAc_C[k] = VTA_da.Conc_DA_term
+    NU[k] = VTA_da.nu
+    
+plt.figure(1)
+plt.subplot(2,1,1)
+plt.plot(t, VTA_C)
+plt.title('VTA-NAc dopamine under constant firing rate')
+plt.ylabel('Somatodendritic DA (nM)')
+
+
+plt.subplot(2,1,2)
+plt.plot(t, NAc_C)
+plt.ylabel('Terminal DA (nM)')
+plt.xlabel('Time (s)');
+
+plt.figure(2)
+plt.plot(t, NU)
