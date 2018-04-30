18088ad0139aa5be125b15026e91553af2df3928
Jakob Kisbye Dreyer <jakd@lundbeck.com>
2018-04-30 17:14:36 +0200

    Renamed toolbox in files reader.

--- source/testfiles/testFileReader.py
+++ source/testfiles/testFileReader.py
@@ -5,12 +5,12 @@ Created on Tue Apr 10 14:00:32 2018
 @author: jakd
 """
 
-from DopamineNeuronClass import AnalyzeSpikesFromFile
+from DopamineToolbox import AnalyzeSpikesFromFile
 
-
+"The file to be opened"
 chrisfile = "M:/Python/Datafiles/644301/644297.txt"
 #chrisfile = "M:/Python/Datafiles/644301/632822.txt"
-RES = AnalyzeSpikesFromFile(chrisfile, pre_run = 00, tmax = 10)
+RES = AnalyzeSpikesFromFile(chrisfile, pre_run = 10, tmax = 10)
 
 print('printing results:')
 print(RES)
